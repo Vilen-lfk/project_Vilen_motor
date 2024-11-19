32a0b7f728a42f04d62edb14a7a9605852ef7184
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-19 19:34:14 +0300

    motor_4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* Страница BMW 1000r*/
+router.get('/bmw', function(req, res, next) {
+  res.send("<h1>Страница BMW 1000r</h1>")
+  });
+  /* Страница Harley davidson */
+router.get('/harley', function(req, res, next) {
+  res.send("<h1>Страница Harley davidson</h1>")
+  });
+  /* Страница Racer 300 */
+router.get('/racer', function(req, res, next) {
+  res.send("<h1>Страница Racer 300</h1>")
+  });
+  
 module.exports = router;
