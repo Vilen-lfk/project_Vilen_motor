7dcde922f7c93ca1fd3bcb9f20f8bc58d3c2d399
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-28 09:09:21 +0300

    motor_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Bike = mongoose.model('Motorcycles', { name: String });
+
+var schema = mongoose.Schema({name : String})
+
+schema.methods.meow = function(){
+    console.log(this.name + " летит 350км/ч")
+    }
+    
+const Bike = mongoose.model('Motorcycles', schema);
 const bmw = new Bike({ name: 'BMW1000R' });
-bmw.save().then(() => console.log('bmw'));
+bmw.save().then(() => bmw.meow());
 
 /*const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
