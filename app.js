ce17e7de21d19408dd9dc78bd478c8df886389c7
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-30 15:28:15 +0300

    motor_8_2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://localhost/tc2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var motos = require('./routes/motos');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/motos', motos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
