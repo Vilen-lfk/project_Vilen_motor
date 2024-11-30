42a9e7fcd1bc5341bb8e84ac10b49227ae6512aa
Vilen-lfk <karamurzayev.amet@mail.ru>
2024-11-30 15:54:47 +0300

    motor_8_3

--- routes/motos.js
+++ routes/motos.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Moto = require('../models/bmw').Moto;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с motos');
-});
-/* Страница мотоциклов */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var mots = await Moto.find({nick: req.params.nick});
+    console.log(mots)
+    if(!mots.length) return next(new Error("Нет такого мотоцикла"))
+    var moto = mots[0];
+    res.render('Moto', {
+    title: moto.title,
+    picture: moto.avatar,
+    desc: moto.desc
+    })
     });
+    
 
 module.exports = router;
